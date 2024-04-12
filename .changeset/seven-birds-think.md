2b799f316eef2921e4585385256888efc39536bb
Vitali Haradkou <vitalicset@yandex.ru>
2024-04-12 10:41:59 +0300

    fix: peer dependency package (#6)
    * fix: peer dependency usage
    feat: add free memory metric
    * add changeset

---
+++ .changeset/seven-birds-think.md
@@ -0,0 +1,7 @@
+---
+"playwright-prometheus-remote-write-reporter": patch
+---
+
+Fix peer dependency.
+
+Add free memory metric for node.js internals.
--- example/tests/example.spec.ts
+++ example/tests/example.spec.ts
@@ -1,38 +1,43 @@
-import { setTimeout } from 'node:timers/promises'
-import { test, expect } from '@playwright/test';
-import { Counter } from 'playwright-prometheus-remote-writer-reporter'
+import { setTimeout } from "node:timers/promises";
+import { test, expect } from "@playwright/test";
+import { Counter } from "playwright-prometheus-remote-write-reporter";
 
-const goesCounter = new Counter({
-  name: 'url',
-  url: 'playwright.dev'
-}, 1)
+const goesCounter = new Counter(
+  {
+    name: "url",
+    url: "playwright.dev",
+  },
+  1,
+);
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
   goesCounter.inc().collect();
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
-  console.log('Good for you!')
+  console.log("Good for you!");
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
   goesCounter.inc().collect();
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
-  await setTimeout(10_000)
+  await setTimeout(10_000);
 
-  await test.info().attach('attachment.json', {
-    contentType: 'application/json',
-    body: JSON.stringify({ key: 'value' })
-  })
+  await test.info().attach("attachment.json", {
+    contentType: "application/json",
+    body: JSON.stringify({ key: "value" }),
+  });
 
   test.info().annotations.push({
-    type: 'custom annotation',
-    description: 'my custom description'
-  })
+    type: "custom annotation",
+    description: "my custom description",
+  });
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" }),
+  ).toBeVisible();
 });
